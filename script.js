35b944abb30ab2dedde87ceec3c1ee5fbed4e59d
Danuz05 <137125134+Danuz05@users.noreply.github.com>
2023-08-02 22:35:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const full = document.getElementById("full")
+const full = document.querySelector(".img2")
+const full2 = document.querySelector(".skills2")
+const closeimg = document.querySelector(".close-img")
+
+closeimg.addEventListener("click",()=>{
+full2.classList.remove("skills2-active")
+})
+
+full.addEventListener("click",()=>{
+    full2.classList.toggle("skills2-active")
+})
