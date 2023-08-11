b4ac9bb3886606de5bc01b4f005033492522338b
Danuz05 <137125134+Danuz05@users.noreply.github.com>
2023-08-11 15:20:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const full = document.querySelector(".img2")
 const full2 = document.querySelector(".skills2")
 const closeimg = document.querySelector(".close-img")
+const cambiarFondoBtn = document.getElementById('cambiarFondoBtn');
+const cambiarFondoBtn2 = document.querySelector("#cambiarFondoBtn-2")
+const fondos = document.getElementById("fondo")
+const main = document.querySelector(".main")
 
 closeimg.addEventListener("click", () => {
     full2.classList.remove("skills2-active")
@@ -10,6 +14,7 @@ full.addEventListener("click", () => {
     full2.classList.toggle("skills2-active")
 })
 
-full2.addEventListener("click",()=>{
+full2.addEventListener("click", () => {
     full2.classList.remove("skills2-active")
 })
+
